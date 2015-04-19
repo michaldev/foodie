d92d13d80b18bde6ecaa40552b0dcd682cbe22c0
Michal <michal0468@gmail.com>
2015-04-19 22:26:34 +0200

    poprawka

--- basefood/serializers.py
+++ basefood/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import Category, CategoryMain
 
 class CategorySerializer(serializers.ModelSerializer):
+    categorymain = serializers.StringRelatedField(many=True)
+
     class Meta:
         model = Category
         fields = ('id', 'name', 'slug', 'categorymain')

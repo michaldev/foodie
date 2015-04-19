29a14db4658046d19e173c810bddfa59960f5cd3
Michal <michal0468@gmail.com>
2015-04-19 22:38:18 +0200

    Poprawki w api

--- basefood/serializers.py
+++ basefood/serializers.py
@@ -1,14 +1,14 @@
 from rest_framework import serializers
 from .models import Category, CategoryMain
 
-class CategorySerializer(serializers.ModelSerializer):
-    categorymain = serializers.StringRelatedField(many=True)
-
-    class Meta:
-        model = Category
-        fields = ('id', 'name', 'slug', 'categorymain')
-
 class CategoryMainSerializer(serializers.ModelSerializer):
     class Meta:
         model = CategoryMain
         fields = ('id', 'name', 'slug')
+
+class CategorySerializer(serializers.ModelSerializer):
+    categorymain = CategoryMainSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Category
+        fields = ('id', 'name', 'slug', 'categorymain')

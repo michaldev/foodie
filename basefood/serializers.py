641accfe58910cb89c2cac9114bf5dc2578eb746
Michal <michal0468@gmail.com>
2015-04-19 23:36:25 +0200

    kilka poprawek

--- basefood/serializers.py
+++ basefood/serializers.py
@@ -1,39 +1,49 @@
+# -*- coding: utf-8 -*-
 from rest_framework import serializers
 from .models import Category, CategoryMain, Product, Vitamin, Mineral, Preservative, Shop
+
 
 class CategoryMainSerializer(serializers.ModelSerializer):
     class Meta:
         model = CategoryMain
         fields = ('id', 'name', 'slug')
 
+
 class CategorySerializer(serializers.ModelSerializer):
+    """Główna serializacja kategorii"""
     categorymain = CategoryMainSerializer(many=True, read_only=True)
 
     class Meta:
         model = Category
         fields = ('id', 'name', 'slug', 'categorymain')
 
+
 class VitaminSerializer(serializers.ModelSerializer):
     class Meta:
         model = Vitamin
         fields = ('id', 'name', 'slug', 'description')
+
 
 class MineralSerializer(serializers.ModelSerializer):
     class Meta:
         model = Mineral
         fields = ('id', 'name', 'slug', 'description')
 
+
 class PreservativeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Preservative
         fields = ('id', 'name', 'slug', 'description', 'level')
+
 
 class ShopSerializer(serializers.ModelSerializer):
     class Meta:
         model = Shop
         fields = ('id', 'name', 'slug', 'image')
 
+
 class ProductSerializer(serializers.ModelSerializer):
+    """Główna serializacja produktu"""
     category = CategorySerializer(many=True, read_only=True)
     vitamins = VitaminSerializer(many=True, read_only=True)
     minerals = MineralSerializer(many=True, read_only=True)
@@ -43,5 +53,6 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ('id', 'name', 'producer', 'slug', 'image', 'image2', 'category', 'sugar', 'size',
-        	'protein', 'vitamins', 'minerals', 'carbohydrates', 'fats', 'fatsSaturated', 'energyValue', 
-        	'portion', 'preservatives', 'shops', 'pricemin', 'pricemax')
+            'protein', 'vitamins', 'minerals', 'carbohydrates', 'fats', 'fatsSaturated', 'energyValue', 
+            'portion', 'preservatives', 'shops', 'pricemin', 'pricemax')
+

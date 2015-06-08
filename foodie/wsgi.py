ff0220dd47e2011b2dd1378f4aa9240573249a02
Michal <michal0468@gmail.com>
2015-06-08 13:41:34 +0200

    fix

--- foodie/wsgi.py
+++ foodie/wsgi.py
@@ -1,6 +1,6 @@
 import os
 import sys
 sys.path.append('/var/www/foodie/')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'deployment.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'foodie.settings'
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()

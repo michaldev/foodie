e32e88d509494f8a7fd62ff137584dc6d82c8876
Arth <axor95@gmail.com>
2015-06-08 20:49:34 +0200

    Drobne poprawki

--- main/static/js/myApp.js
+++ main/static/js/myApp.js
@@ -8,8 +8,29 @@ myApp.config(function($httpProvider){
     $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
 });
 myApp.config(function($mdThemingProvider) {
+	$mdThemingProvider.definePalette('customGreen', {
+    "50": "#edf7ee",
+    "100": "#c9e7ca",
+    "200": "#a6d7a8",
+    "300": "#3D9C40",
+    "400": "#6abc6d",
+    "500": "#4caf50",
+    "600": "#439946",
+    "700": "#39833c",
+    "800": "#306d32",
+    "900": "#265828",
+    "A100": "#c9e7ca",
+    "A200": "#a6d7a8",
+    "A400": "#6abc6d",
+    "A700": "#39833c",
+    'contrastDefaultColor': 'light',    // whether, by default, text (contrast)
+                                        // on this palette should be dark or light
+    'contrastDarkColors': ['50', '100', //hues which contrast should be 'dark' by default
+     '200', '300', '400', 'A100'],
+    'contrastLightColors': undefined    // could also specify this if default was 'dark'
+});
   $mdThemingProvider.theme('default')
-    .primaryPalette('green')
+    .primaryPalette('customGreen')
     .accentPalette('deep-orange');
 });
 

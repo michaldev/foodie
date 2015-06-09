fe8ec451ce5992b113225aed7b8bdb5e80a47ba0
Michal <michal0468@gmail.com>
2015-06-09 16:36:48 +0200

    poprawa api

--- basefood/urls.py
+++ basefood/urls.py
@@ -1,23 +1,27 @@
 from django.conf.urls import patterns, include, url
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from .views import CategoryList, Product, VitaminList, PreservativeList, ProductList
+from .views import CategoryList, Product, VitaminList, PreservativeList, ProductList, ProductSearch
 
 urlpatterns = patterns('basefood.views',
     url(
-        regex = r'^/category', 
+        regex = r'^/category$', 
         view = CategoryList.as_view(),
         name = 'basefood-category-list'),
     url(
-        regex = r'^/vitamins', 
+        regex = r'^/vitamins$', 
         view = VitaminList.as_view(),
         name = 'basefood-vitamin-list'),
     url(
-        regex = r'^/products', 
+        regex = r'^/products$', 
         view = ProductList.as_view(),
         name = 'basefood-product-list'),
     url(
-        regex = r'^/preservatives', 
+        regex = r'^/productsearch$', 
+        view = ProductSearch.as_view(),
+        name = 'basefood-product-search'),
+    url(
+        regex = r'^/preservatives$', 
         view = PreservativeList.as_view(),
         name = 'basefood-preservative-list'),
     url(

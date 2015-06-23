4b710cbb5cab3630599392d36d2bed31d7b51b02
Michal <michal0468@gmail.com>
2015-06-23 11:46:37 +0200

    Dodanie drawa appki mobilnej

---
+++ aplikacja_mobilna/main.py
@@ -0,0 +1,31 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.tabbedpanel import TabbedPanel
+
+
+class HelloApp(App):
+    pass
+
+
+class GlownyElement(BoxLayout):
+    pass
+
+
+class Taby(TabbedPanel):
+    pass
+
+
+class Stopka(BoxLayout):
+    pass
+
+
+class Widok(BoxLayout):
+    pass
+
+
+class Widok2(BoxLayout):
+    pass
+
+
+if __name__ == '__main__':
+    HelloApp().run()

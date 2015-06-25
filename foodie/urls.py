d687cea0f73d9e0b1fd1f0a20be51ad6e08ba6be
Michal <michal0468@gmail.com>
2015-06-25 12:36:49 +0200

    poprawka wciec

--- foodie/urls.py
+++ foodie/urls.py
@@ -9,36 +9,41 @@ admin.site.site_header = 'Foodie - Panel Admina'
 
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'foodie.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
     url(
-	    regex = r'^$', 
-	    view = Homepage.as_view(),
-	    name = 'home'),
-    url(r'^basefood', include('basefood.urls')),
-    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
-    url(r'^productview', 
+        r'^admin/', include(admin.site.urls)),
+    url(
+        regex = r'^$',
+        view = Homepage.as_view(),
+        name = 'home'),
+    url(
+        r'^basefood', include('basefood.urls')),
+    url(
+        r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    url(
+        r'^productview', 
         view = ProductView.as_view()
         ),
-    url(r'^vitaminview', 
+    url(
+        r'^vitaminview', 
         view = VitaminView.as_view()
         ),
-    url(r'^homeview', 
+    url(
+        r'^homeview',
         view = HomeView.as_view()
         ),
-    url(r'^aboutview', 
+    url(
+        r'^aboutview',
         view = AboutView.as_view()
         ),
 
-    url(r'^preservativeview', 
+    url(
+        r'^preservativeview',
         view = PreservativeView.as_view()
-        ), 
-    url(r'^categoryview', 
+        ),
+    url(
+        r'^categoryview',
         view = CategoryView.as_view()
-        ), 
+        ),
 )
 
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
